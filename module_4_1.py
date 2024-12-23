fdaf7d7fbd4bbe99b634fdf7d1c1200e92237417
Temushkaaa <puhatema@gmail.com>
2024-12-23 17:17:46 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as DFM
+
+from true_math import divide as DTM
+
+result1 = DFM(69, 3)
+result2 = DFM(3, 0)
+result3 = DTM(49, 7)
+result4 = DTM(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
